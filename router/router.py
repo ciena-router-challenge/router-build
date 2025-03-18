587455e01e24060af4548132631a19ef6f1435c0
Triet Duong <triet.m.duong@gmail.com>
2025-03-18 19:33:47 -0400

    Case 3

--- router/router.py
+++ router/router.py
@@ -11,24 +11,28 @@ def print_cli_history(history):
 
 def process_cli_input(file_path, history, t):
     # Process CLI input here
+
     try:
         user_input = input("Enter CLI command: ")
         command, *args = user_input.split()
         if command == "set":
             index = int(args[0]) - 1
             value = int(args[1])
-            if index < 0 or index >3 :
-                print(f"Invalid Input - Error: {index}")
+            if index < 0 or index > 3 :
+                print(f"Invalid Input - Error: Index {index} out of valid range")
             else:
                 mutate_database(file_path, index, value)
                 history.append(f"{t} set {index} {value}")
+        elif command == "swap":
+            0 = 0
+        else:
+            print(f"Invalid Input - Error: {command} Command does not exist")
     except Exception as e:
         print(f"Invalid Input - Error: {str(e)}")
 
 def main():
     history = []
     t = 0
-
 
     while t < 60:
         state_values, control_values, signal_values = read_hardware_state(file_path)
@@ -38,6 +42,8 @@ def main():
         if (signal_values[0] >= 1 and signal_values[0] <= 4):
             setting = signal_values[0]
             control_values[setting] = signal_values[1] 
+
+        process_cli_input(file_path, history, t)
 
         # Write Your Code Here End
 
