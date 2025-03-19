8fad1a9004b6626ef12cde41c4d4a38b3b917a48
davidkusimo <154037095+davidkusimo@users.noreply.github.com>
2025-03-18 20:21:50 -0400

    final

---
+++ router-build/router/router.py
@@ -0,0 +1,66 @@
+import subprocess
+import sys
+import time
+import select
+
+from simulation import read_hardware_state, write_hardware_state, calculate_f, mutate_hardware, mutate_database, create_hardware_file, file_path
+
+def print_cli_history(history):
+    for entry in history:
+        print(entry)
+
+def process_cli_input(file_path, history, t):
+
+    # Process CLI input here
+
+    try:
+        user_input = input("Enter CLI command: ")
+        command, *args = user_input.split()
+        if command == "set":
+            index = int(args[0]) - 1
+            value = int(args[1])
+            if index < 0 or index > 3 :
+                print(f"Invalid Input - Error: Index {index} out of valid range")
+            else:
+                mutate_database(file_path, index, value)
+                history.append(f"{t} set {index} {value}")
+        else:
+            print(f"Invalid Input - Error: {command} Command does not exist")
+    except Exception as e:
+        print(f"Invalid Input - Error: {str(e)}")
+
+def main():
+    history = []
+    t = 0
+
+
+    while t < 60:
+        state_values, control_values, signal_values = read_hardware_state(file_path)
+        t += 1
+
+        # Write Your Code Here Start
+        if (signal_values[0] >= 1 and signal_values[0] <= 4):
+            setting = signal_values[0]
+            control_values[setting] = signal_values[1] 
+        
+        process_cli_input(file_path, history, t)
+        answer = t," set ", setting, signal_values[1]
+        
+
+        if t%10 == 0:
+            holder = state_values[0]
+            mutate_database(file_path, 0, state_values[1])
+            mutate_database(file_path, 1, holder)
+            answer = t," swap ", state_values[1], state_values[0]
+            history.append(answer)
+
+        print(history)
+             
+
+        # Write Your Code Here End
+
+        time.sleep(1)  # Wait for 1 second before polling again
+    print(history)
+
+if __name__ == '__main__':
+    main()
